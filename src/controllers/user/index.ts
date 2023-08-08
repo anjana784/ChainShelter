cdaa8b44abd50b727827c9398a8680ba99cd3168
anjana784 <hello@anjana784.dev>
2023-08-08 18:51:17 +0530

    added createUser handler

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,8 +1,36 @@
 import type { RequestHandler } from "express";
-import User from "./../../models/user";
+import User from "../../models/User";
+import dbConnect from "./../../utils/dbConnect";
+import errorHandler from "./../../middlewares/errorHandler";
+import AppError from "./../../models/AppError";
 
-export const createUser: RequestHandler = (req, res) => {
+export const createUser: RequestHandler = async (req, res) => {
+  // get the data from the request body
   const { name, email, mobile, password, role } = req.body;
-  const user = new User(name, email, mobile, password, role);
-  res.status(201).json({ user });
+
+  // get the users collection
+  const { usersCollection } = await dbConnect();
+
+  // check if the user already exists
+  const existingUser = await usersCollection.findOne({ email });
+
+  // if the user exists, return an error
+  if (existingUser) {
+    errorHandler(
+      new AppError(400, {
+        type: "Bad Request",
+        message: "User already exists",
+      }),
+      req,
+      res
+    );
+  } else {
+    // create a new user
+    const user = new User(name, email, mobile, password, role);
+
+    // save the user to the database
+    await usersCollection.insertOne(user);
+
+    res.status(201).json({ user });
+  }
 };
---
+++ src/utils/dbConnect.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+const dbConnect = async () => {
+  // instantiate a new client
+  const client = new MongoClient(
+    process.env.DATABASE_URI.replace(
+      "<password>",
+      process.env.DATABASE_PASSWORD
+    )
+  );
+
+  // connect to the client
+  await client.connect();
+
+  // connect to the database
+  const database = client.db(process.env.DATABASE_NAME);
+
+  // get the users collection
+  const usersCollection = database.collection("users");
+
+  // return the users collection
+  return { usersCollection };
+};
+
+export default dbConnect;
